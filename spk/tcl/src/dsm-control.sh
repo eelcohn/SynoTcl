299aee9a52618945800134314ad8a650ececabcd
Synology <synology@synology.localdomain>
2017-04-27 12:50:56 +0200

    SynoTcl v8.6.6-2

--- spk/tcl/src/dsm-control.sh
+++ spk/tcl/src/dsm-control.sh
@@ -3,22 +3,24 @@
 # Package
 PACKAGE="tcl"
 DNAME="Tcl"
+LOG_FILE="/var/packages/${PACKAGE}/target/var/${PACKAGE}-sss.log"
 
 
 case $1 in
-    start)
-        exit 0
-        ;;
-    stop)
-        exit 0
-        ;;
-    status)
-        exit 0
-        ;;
-    log)
-        exit 1
-        ;;
-    *)
-        exit 1
-        ;;
+	start)
+		exit 0
+		;;
+	stop)
+		exit 0
+		;;
+	status)
+		exit 0
+		;;
+	log)
+		echo ${LOG_FILE}
+		exit 1
+		;;
+	*)
+		exit 1
+		;;
 esac
--- spk/tcl/src/installer.sh
+++ spk/tcl/src/installer.sh
@@ -6,6 +6,9 @@ DNAME="Tcl"
 
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
+PACKAGE_DIR="/var/packages/${PACKAGE}/target"
+LOG_FILE="/tmp/${PACKAGE}-sss.log"
+
 
 
 preinst ()
@@ -15,12 +18,22 @@ preinst ()
 
 postinst ()
 {
+	# Create location for log file
+	# mkdir ${PACKAGE_DIR}/var 2>&1
+
+	if [ "${SYNOPKG_DSM_VERSION_MAJOR}" < 6 ]; then
+		# Don't create this symlink on DSM6: it will reference back on itself causing an infinite loop
+		ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR} >> ${LOG_FILE} 2>&1
+	fi
+
+	# On brand new DiskStations with DSM, these locations don't exist yet. Gotta replace this with https://developer.synology.com/developer-guide/resource_acquisition/usrlocal_linker.html
+	mkdir -p /usr/local/bin/ >> ${LOG_FILE} 2>&1
+	mkdir -p /usr/local/lib/ >> ${LOG_FILE} 2>&1
+
 	# Create symlinks
-	ln -s ${SYNOPKG_PKGDEST}/bin/tclsh8.6 /usr/local/bin/tclsh
-	ln -s ${SYNOPKG_PKGDEST}/lib/libtcl8.6.so /usr/local/lib/libtcl.so
-	ln -s ${SYNOPKG_PKGDEST}/lib/libtcl8.6.so /usr/local/lib/libtcl8.6.so
-	ln -s ${SYNOPKG_PKGDEST}/lib/tcl8.6/ /usr/local/lib/tcl8.6
-	export LD_LIBRARY_PATH=/usr/local/lib/
+	ln -s ${PACKAGE_DIR}/bin/tclsh8.6 /usr/local/bin/tclsh >> ${LOG_FILE} 2>&1
+	ln -s ${PACKAGE_DIR}/lib/libtcl8.6.so /usr/local/lib/libtcl.so >> ${LOG_FILE} 2>&1
+	ln -s ${PACKAGE_DIR}/lib/libtcl8.6.so /usr/local/lib/libtcl8.6.so >> ${LOG_FILE} 2>&1
 
 	exit 0
 }
@@ -33,10 +46,13 @@ preuninst ()
 postuninst ()
 {
 	# Remove symlinks
+	if [ "${SYNOPKG_DSM_VERSION_MAJOR}" < 6 ]; then
+		rm -f ${INSTALL_DIR}
+	fi
+
 	rm -f /usr/local/bin/tclsh
 	rm -f /usr/local/lib/libtcl.so
 	rm -f /usr/local/lib/libtcl8.6.so
-	rm -f /usr/local/lib/tcl8.6
 
 	exit 0
 }
