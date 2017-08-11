715de2e7e51517fa27006e6f75a0be1972bee52c
eelcohn <eelco@huininga.nl>
2017-08-11 08:23:28 +0200

    Added local.mk with custom settings for INFO file

---
+++ local.mk
@@ -0,0 +1,8 @@
+PUBLISH_URL=https://github.com/eelcohn/SynoTcl/releases
+PUBLISH_API_KEY=
+MAINTAINER=Tcl developer Xchange
+MAINTAINER_URL=https://www.tcl.tk/
+DISTRIBUTOR=eelcohn
+DISTRIBUTOR_URL=https://github.com/eelcohn/SynoTcl/
+REPORT_URL=https://github.com/eelcohn/SynoTcl/issues/
+DEFAULT_TC=5.2
