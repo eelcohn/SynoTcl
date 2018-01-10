ab489cd3e5c1ecb2d72c4ec1439c6552481e9a8d
eelcohn <eelco@huininga.nl>
2018-01-10 10:19:31 +0100

    Create tcltests.tcl

---
+++ tcltests.tcl
@@ -0,0 +1,11 @@
+clock scan 1752-09-02 -format %Y-%m-%d -locale en_US
+puts $tcl_pkgPath
+puts $auto_path
+package require itcl
+package require sqlite3
+package require tdbc
+package require tdbc::mysql
+package require tdbc::odbc
+package require tdbc::postgres
+package require sqlite3
+package require Thread
